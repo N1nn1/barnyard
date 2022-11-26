286ced724a762ebee144578a8af41c35a8b1dad5
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-11-27 00:48:27 +0100

    Pig textures

--- src/main/java/com/ninni/barnyard/client/render/PigRenderer.java
+++ src/main/java/com/ninni/barnyard/client/render/PigRenderer.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 @Environment(value= EnvType.CLIENT)
 public class PigRenderer extends MobRenderer<Pig, PigModel> {
     private static final ResourceLocation PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/pig.png");
+    private static final ResourceLocation TUSKED_PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/tusked_pig.png");
 
     public PigRenderer(EntityRendererProvider.Context context) {
         super(context, new PigModel(context.bakeLayer(BarnyardModelLayers.PIG)), 0.7f);
