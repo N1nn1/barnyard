d5b4ff8bf41760442be33991380cde29c270a79b
0rc1nus <mitchellctlui@gmail.com>
2022-11-27 09:48:29 +0800

    Added tusk variant

--- src/main/java/com/ninni/barnyard/client/render/PigRenderer.java
+++ src/main/java/com/ninni/barnyard/client/render/PigRenderer.java
@@ -4,7 +4,6 @@ import com.ninni.barnyard.Barnyard;
 import com.ninni.barnyard.client.model.BarnyardModelLayers;
 import com.ninni.barnyard.client.model.PigModel;
 import com.ninni.barnyard.entities.BarnyardPig;
-import com.ninni.barnyard.entity.Pig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
@@ -12,7 +11,7 @@ import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
-@Environment(value= EnvType.CLIENT)
+@Environment(value=EnvType.CLIENT)
 public class PigRenderer extends MobRenderer<BarnyardPig, PigModel> {
     private static final ResourceLocation PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/pig.png");
     private static final ResourceLocation TUSKED_PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/tusked_pig.png");
@@ -23,7 +22,7 @@ public class PigRenderer extends MobRenderer<BarnyardPig, PigModel> {
 
     @Override
     public @NotNull ResourceLocation getTextureLocation(BarnyardPig entity) {
-        return PIG_LOCATION;
+        return entity.hasTusk() ? TUSKED_PIG_LOCATION : PIG_LOCATION;
     }
 }
 
--- src/main/java/com/ninni/barnyard/entities/BarnyardPig.java
+++ src/main/java/com/ninni/barnyard/entities/BarnyardPig.java
@@ -5,11 +5,18 @@ import com.mojang.serialization.Dynamic;
 import com.ninni.barnyard.entities.ai.BarnyardPigAi;
 import com.ninni.barnyard.init.BarnyardEntityTypes;
 import com.ninni.barnyard.init.BarnyardSensorTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.game.DebugPackets;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.ai.Brain;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -18,14 +25,50 @@ import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import org.jetbrains.annotations.Nullable;
 
 public class BarnyardPig extends Animal {
     protected static final ImmutableList<SensorType<? extends Sensor<? super BarnyardPig>>> SENSOR_TYPES = ImmutableList.of(SensorType.NEAREST_LIVING_ENTITIES, SensorType.NEAREST_PLAYERS, SensorType.NEAREST_ITEMS, SensorType.NEAREST_ADULT, SensorType.HURT_BY, BarnyardSensorTypes.PIG_TEMPTATIONS);
     protected static final ImmutableList<MemoryModuleType<?>> MEMORY_TYPES = ImmutableList.of(MemoryModuleType.LOOK_TARGET, MemoryModuleType.NEAREST_VISIBLE_LIVING_ENTITIES, MemoryModuleType.WALK_TARGET, MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE, MemoryModuleType.PATH, MemoryModuleType.ATE_RECENTLY, MemoryModuleType.BREED_TARGET, MemoryModuleType.TEMPTING_PLAYER, MemoryModuleType.NEAREST_VISIBLE_ADULT, MemoryModuleType.TEMPTATION_COOLDOWN_TICKS, MemoryModuleType.IS_TEMPTED, MemoryModuleType.IS_PANICKING);
+    private static final EntityDataAccessor<Boolean> TUSK = SynchedEntityData.defineId(BarnyardPig.class, EntityDataSerializers.BOOLEAN);
 
     public BarnyardPig(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(TUSK, false);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compoundTag) {
+        super.addAdditionalSaveData(compoundTag);
+        compoundTag.putBoolean("Tusk", this.hasTusk());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compoundTag) {
+        super.readAdditionalSaveData(compoundTag);
+        this.setHasTusk(compoundTag.getBoolean("Tusk"));
+    }
+
+    public boolean hasTusk() {
+        return this.entityData.get(TUSK);
+    }
+
+    public void setHasTusk(boolean tusk) {
+        this.entityData.set(TUSK, tusk);
+    }
+
+    @Override
+    public SpawnGroupData finalizeSpawn(ServerLevelAccessor serverLevelAccessor, DifficultyInstance difficultyInstance, MobSpawnType mobSpawnType, @Nullable SpawnGroupData spawnGroupData, @Nullable CompoundTag compoundTag) {
+        if (serverLevelAccessor.getRandom().nextFloat() < 0.2f) {
+            this.setHasTusk(true);
+        }
+        return super.finalizeSpawn(serverLevelAccessor, difficultyInstance, mobSpawnType, spawnGroupData, compoundTag);
     }
 
     public static AttributeSupplier.Builder createAttributes() {
