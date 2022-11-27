3979c67123aa0f48ae3c10590fb735e91e259c27
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-11-27 23:45:02 +0100

    muddy pig textures

--- src/main/java/com/ninni/barnyard/client/render/BarnyardPigRenderer.java
+++ src/main/java/com/ninni/barnyard/client/render/BarnyardPigRenderer.java
@@ -6,8 +6,6 @@ import com.ninni.barnyard.client.model.BarnyardPigModel;
 import com.ninni.barnyard.entities.BarnyardPig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.model.PigModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.SaddleLayer;
@@ -18,11 +16,13 @@ import org.jetbrains.annotations.NotNull;
 public class BarnyardPigRenderer extends MobRenderer<BarnyardPig, BarnyardPigModel> {
     private static final ResourceLocation PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/pig.png");
     private static final ResourceLocation TUSKED_PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/tusked_pig.png");
+    private static final ResourceLocation MUDDY_PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/muddy_pig.png");
+    private static final ResourceLocation MUDDY_TUSKED_PIG_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/muddy_tusked_pig.png");
     private static final ResourceLocation SADDLE_LOCATION = new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/pig_saddle.png");
 
     public BarnyardPigRenderer(EntityRendererProvider.Context context) {
         super(context, new BarnyardPigModel(context.bakeLayer(BarnyardModelLayers.PIG)), 0.6f);
-        this.addLayer(new SaddleLayer<>(this, new BarnyardPigModel(context.bakeLayer(BarnyardModelLayers.PIG_SADDLE)), new ResourceLocation(Barnyard.MOD_ID, "textures/entity/pig/pig_saddle.png")));
+        this.addLayer(new SaddleLayer<>(this, new BarnyardPigModel(context.bakeLayer(BarnyardModelLayers.PIG_SADDLE)), SADDLE_LOCATION));
     }
 
     @Override
