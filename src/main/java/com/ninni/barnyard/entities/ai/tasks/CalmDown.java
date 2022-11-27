d5c04d8dd6d5a358706f4d4d1512f4e3f0c999f5
buntennn <bunten2005@gmail.com>
2022-11-27 16:00:50 -0600

    fix calm down i think

--- src/main/java/com/ninni/barnyard/entities/ai/tasks/CalmDown.java
+++ src/main/java/com/ninni/barnyard/entities/ai/tasks/CalmDown.java
@@ -6,22 +6,23 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.behavior.Behavior;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
+import net.minecraft.world.entity.ai.memory.MemoryStatus;
 
 public class CalmDown extends Behavior<LivingEntity> {
 
     public CalmDown() {
-        super(ImmutableMap.of());
+        super(ImmutableMap.of(MemoryModuleType.HURT_BY_ENTITY, MemoryStatus.VALUE_PRESENT));
     }
 
     @Override
     protected void start(ServerLevel level, LivingEntity mob, long l) {
-        if (mob.getBrain().hasMemoryValue(MemoryModuleType.HURT_BY) && !isNearAttacker(mob)) {
+        if (!isNearAttacker(mob)) {
             mob.getBrain().eraseMemory(MemoryModuleType.HURT_BY);
             mob.getBrain().eraseMemory(MemoryModuleType.HURT_BY_ENTITY);
         }
     }
 
     private static boolean isNearAttacker(LivingEntity mob) {
-        return mob.getBrain().getMemory(MemoryModuleType.HURT_BY_ENTITY).filter(next -> mob.distanceToSqr(next) <= 32).isPresent();
+        return mob.getBrain().getMemory(MemoryModuleType.HURT_BY_ENTITY).filter(next -> mob.distanceToSqr(next) <= 48).isPresent();
     }
 }
