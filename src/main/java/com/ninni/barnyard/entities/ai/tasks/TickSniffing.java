3bdf05fdf3b331ee17c38b5fd2d7b67770343291
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-11-28 01:21:50 +0100

    Digging loot table

--- src/main/java/com/ninni/barnyard/entities/ai/tasks/TickSniffing.java
+++ src/main/java/com/ninni/barnyard/entities/ai/tasks/TickSniffing.java
@@ -3,22 +3,29 @@ package com.ninni.barnyard.entities.ai.tasks;
 import com.google.common.collect.ImmutableMap;
 import com.ninni.barnyard.Barnyard;
 import com.ninni.barnyard.entities.BarnyardPig;
-import com.ninni.barnyard.init.BarnyardItems;
 import com.ninni.barnyard.init.BarnyardMemoryModules;
 
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Unit;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.behavior.Behavior;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.minecraft.world.entity.ai.memory.MemoryStatus;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.phys.Vec3;
 
+import java.util.List;
+
+import static com.ninni.barnyard.Barnyard.MOD_ID;
+
 public class TickSniffing extends Behavior<BarnyardPig> {
+    private static final ResourceLocation DIGGING_LOOT = new ResourceLocation(MOD_ID, "entities/pig_digging");
 
     public TickSniffing(int duration) {
         super(ImmutableMap.of(MemoryModuleType.IS_SNIFFING, MemoryStatus.VALUE_PRESENT, MemoryModuleType.SNIFF_COOLDOWN, MemoryStatus.VALUE_ABSENT, BarnyardMemoryModules.PIG_SNIFFING_TICKS, MemoryStatus.VALUE_PRESENT, MemoryModuleType.WALK_TARGET, MemoryStatus.VALUE_ABSENT, MemoryModuleType.LOOK_TARGET, MemoryStatus.REGISTERED), duration);
@@ -39,7 +46,9 @@ public class TickSniffing extends Behavior<BarnyardPig> {
                 level.sendParticles(new BlockParticleOption(ParticleTypes.BLOCK, pig.getBlockStateOn()), pig.getX() + look.x(), pig.getY() + 0.1, pig.getZ() + look.z(), 1, 0.2, 0.1, 0.2, 0);
             }
             if (time == 38) {
-                ItemEntity item = new ItemEntity(level, pig.getX() + look.x(), pig.getY() + 0.2, pig.getZ() + look.z(), BarnyardItems.TRUFFLE.getDefaultInstance());
+
+                List<ItemStack> items = level.getServer().getLootTables().get(DIGGING_LOOT).getRandomItems(new LootContext.Builder(level).withRandom(level.getRandom()).create(LootContextParamSets.EMPTY));
+                ItemEntity item = new ItemEntity(level, pig.getX() + look.x(), pig.getY() + 0.2, pig.getZ() + look.z(), items.get(0));
                 item.setDefaultPickUpDelay();
                 item.setDeltaMovement(look.multiply(0.3, 0, 0.3).add(0, 0.15, 0));
                 level.addFreshEntity(item);
